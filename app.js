a0fe3d96c0c6da5a2d3b5a7a89e06482ecc72bf5
J0K3Rn <JosephKeeganRichardson@gmail.com>
2023-03-01 16:43:45 -0600

    Level 2.5 - Adding environment file
    Pushing .env file for reference

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
         - Fill out database credentials in the 'Database Setup' section for MongoDB Atlas
 */
 
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -33,8 +34,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] } );
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] } );
 
 const User = mongoose.model("User", userSchema);
 
