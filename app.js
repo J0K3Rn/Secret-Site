f73392eccb19534d3ede84a6c7e98ef479ccf656
J0K3Rn <JosephKeeganRichardson@gmail.com>
2023-03-01 17:17:16 -0600

    Level 3 - Hashing Passwords
    Utilized md5 encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 /*
     ToDo
         - Fill out database credentials in the 'Database Setup' section for MongoDB Atlas
+        - Uncomment .env from gitignore
 */
 
 require('dotenv').config();
@@ -13,7 +14,7 @@ const ejs = require("ejs");
 // Database libraries
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 // Database Setup
 const database = 'userDB';
@@ -33,8 +34,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] } );
 
 const User = mongoose.model("User", userSchema);
 
@@ -75,7 +74,7 @@ app.post("/submit", function (req, res) {
 app.post("/register", function (req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save().then(() => {
@@ -88,7 +87,7 @@ app.post("/register", function (req, res) {
 app.post("/login", async function (req, res) {
 
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     const foundUser = await User.findOne({
         email: username
